16091b5c676482373b50c8ec37888ef8cc751344
Jonathan Igner <jonathan.igner@vw.com>
2018-08-01 13:31:20 -0700

    Add Stripe Checkout, live mode with serverless functions

--- functions/stripe-charge.js
+++ functions/stripe-charge.js
@@ -1,3 +1,26 @@
-exports.handler = function() {
-    console.log('working...')
+const stripe = require('stripe')('sk_live_caIr0bhy3Cdzsk1wxEvrNjY9')
+
+// Uses an async function call
+exports.handler = async function(event) {
+    const {
+        tokenId,
+        email,
+        name,
+        description,
+        amount
+    } = JSON.parse(event.body)
+
+    // Must create customer first before running next func
+    const customer = await stripe.customers.create({
+        description: email,
+        source: tokenId
+    })
+
+    await stripe.charges.create({
+        customer: customer.id,
+        amount,
+        name,
+        description,
+        currency: 'usd'
+    })
 }
--- src/components/header.js
+++ src/components/header.js
@@ -31,6 +31,24 @@ class Header extends React.Component {
     }
   }
 
+  // Stripe purchase logic
+  handlePurchase = (token) => {
+    const amount = 5000
+    const description = "Design goodies"
+    const bodyObject = {
+      tokenId: token.id,
+      email: token.email,
+      name: token.name,
+      description,
+      amount
+    }
+
+    fetch('http://localhost:9000/stripe-charge', {
+      method: 'POST',
+      body: JSON.stringify(bodyObject)
+    })
+  }
+
   render() {
     return (
       <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
@@ -39,9 +57,13 @@ class Header extends React.Component {
           <Link to="/courses">Courses</Link>
           <Link to="/downloads">Downloads</Link>
           <Link to="/workshops">Workshops</Link>
+          {/* Need this as it's a stateful React component */}
           <StripeCheckout
             amount={5000}
-            image="https://drh2acu5z204m.cloudfront.net/items/0U2H3c3u2J3W1f140q1J/logo_iggyigner.png">
+            image="https://drh2acu5z204m.cloudfront.net/items/0U2H3c3u2J3W1f140q1J/logo_iggyigner.png"
+            token={this.handlePurchase}
+            stripeKey={'pk_live_clFiAWkchCibKzZDoUriFtCi'}
+            >
             <button>Buy</button>
           </StripeCheckout>
         </div>
