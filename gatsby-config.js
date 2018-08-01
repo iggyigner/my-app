1606082f85594a960130b98127d56cdbb3d1b5ff
Jonathan Igner <jonathan.igner@vw.com>
2018-07-31 20:57:50 -0700

    Add footer populated by Contentful and GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'Iggy Learns React',
+    description: '1PW container for all code and design projects for Jonathan Iggy Igner.',
+    keywords: 'design technologist, ux design, ui design, react'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 's48byjlhwmwj',
+        accessToken: '78673c74a3320bb53f408f5d2d604845f388f54dfb7236080e6cf926fb6b2144'
+      }
+    }
+  ],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,18 +4,24 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/header'
 import './index.css'
+import Footer from '../components/Footer';
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 
+        data.site.siteMetadata.description },
+        { name: 'keywords', content: 
+        data.site.siteMetadata.keywords },
       ]}
     />
     <Header />
     {children()}
+    <Footer data={data}>
+    Blobs in Cinema 4D, isometric in AI and PS, site in React. <a href="mailto:iggy@iggyigner.com">Email me</a> to ask anything. &copy; 2018.
+    </Footer>
   </div>
 )
 
@@ -25,11 +31,23 @@ Layout.propTypes = {
 
 export default Layout
 
+// cannot create GraphQL queries inside components 
 export const query = graphql`
   query SiteTitleQuery {
     site {
       siteMetadata {
         title
+        description
+        keywords
+      }
+    }
+    allContentfulLink(sort: {fields: [createdAt], order: ASC }) {
+      edges {
+        node {
+          title
+          url
+          createdAt
+        }
       }
     }
   }
